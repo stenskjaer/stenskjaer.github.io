34d056e8d7d703bc8300d301c8e3574045988ebd
Michael S. Christensen <michael.stenskjaer@gmail.com>
2015-05-17 13:21:27 +0200

    Remove old obsolete functions

--- conversion/csv2html.py
+++ conversion/csv2html.py
@@ -300,96 +300,9 @@ def create_main_tex_file(directory, output, compilation, title):
     shutil.rmtree(d)
 
 
-def create_main_html_file(directory, output):
-    """Creates a main html file in directory with same name as var
-    filename.
-
-    Returns the latex document as a string.
-
-    Keyword Arguments:
-    content  -- String formatted as latex. Document content.
-    filename --
-    """
-
-    # First: List all csv-files in input directory
-    content_list = list_csv_files(directory)
-    
-    # Then read the csv-files
-    for item in content_list:
-        
-        # Create temp directory
-        d = output
-        if not os.path.exists(d):
-            os.mkdir(d)
-        
-        # Create .html-file 
-        basename = os.path.basename(item[:-4])
-        filename = os.path.join(d, basename + '.html')
-
-        frames = csv_to_html_frames(item)
-
-        with open(filename, 'w') as f:
-            html_pre = settings.html_pre
-            html_post = settings.html_post
-            complete_content = html_pre + frames + html_post
-            f.write(complete_content)
-
-        os.rename(filename,
-                  os.path.join(output, os.path.basename(filename)))
-
 def compile_latex(filename):
     args = ['latexmk', '-pdf', filename]
     subprocess.call(args, cwd=os.path.dirname(filename))
-
-
-def set_font_html(*args):
-    """Convert input to HTML compliant output:
-    
-    Keyword Arguments: *args -- any amount of input strings returned
-    in list, reformated.
-    """
-
-    output = []
-    for string in args:
-        # Set boldface
-        string = re.sub(r'\*([^\*]+)\*', r'<strong>\1</strong>', string)
-
-        # Set quotation marks
-        string = re.sub(r'\'([^\']+)\'', r"&lsquo;\1&rsquo;", string)
-
-        # Set en-hyphens
-        string = string.replace('--', '–')
-        
-        # Set ldots
-        string = string.replace('...', '&hellip;')
-
-        # Set linebreaks
-        string = string.replace('\\\\', '<br />')
-        
-        output.append(string)
-
-    return(output)
-
-def fontify_tex(string):
-    """ Convert input to LaTeX compliant output:
-    *content* -> \textbf{content};
-    'content' -> `content';
-    ... -> \ldots
-
-    Keyword Arguments:
-    string -- The input string.
-    """
-
-    # Set boldface
-    # string = re.sub(r'\*([^\*]+)\*', r'\\textbf{\1}', string)
-
-    # Set quotation marks
-    string = re.sub(r'\'([^\']+)\'', r"`\1'", string)
-
-    # Set ldots
-    string = re.sub(r'\.{3}', r'\ldots{}', string)
-
-    return(string)
 
     
 def csv_to_frames(filename):
@@ -428,60 +341,6 @@ def csv_to_frames(filename):
 
     return frames
 
-def csv_to_html_frames(filename):
-    """Put cells of csv in html frames and extract slide title from csv
-
-    Return string: frames
-
-    Keyword Arguments:
-    filename -- input file
-    """
-
-
-    with open(filename) as f:
-        # Get the file length
-        length = sum(1 for row in f) + 1
-
-        f.seek(0)
-        reader = csv.reader(f)
-        frames = ""
-                
-        # Get title from first row
-        title = reader.next()[0]
-        
-        # Get the question-answer sets and return html
-        for i, row in enumerate(reader):
-            for cell in row:
-                unicode(cell, 'utf-8')
-            question = row[0]
-            answer = row[1]
-            location = str(i + 1) + ' / ' + str(length - 2)
-
-
-            # Convert *content* to \textbf{content}
-            question, answer, title = set_font_html(question, answer, title)
-
-            # Create html
-            frames += str("""
-            <figure class="question">
-            <h4>Spørgsmål</h4>
-            <p>{0}</p>
-            <span class="title">{1}</span>
-            <span class="location">{2}</span>
-            <div class="close"><a href="../../index.html">&#10005;</a></div>
-            </figure>
-            """.format(question, title, location))
-            frames += str("""
-            <figure class="answer">
-            <h4>Svar</h4>
-            <p>{0}</p>
-            <span class="title">{1}</span>
-            <span class="location">{2}</span>
-            <div class="close"><a href="../../index.html">&#10005;</a></div>
-            </figure>
-            """.format(answer, title, location))
-
-    return frames
 
 def __main__():
     import argparse
